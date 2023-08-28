7420fa8a2441d825e127aeb669433f064ed2856c
Alves <gfernandes@ges.inatel.br>
2023-08-28 15:06:24 -0300

    28/08

--- lib/edit_perfil_user.dart
+++ lib/edit_perfil_user.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, prefer_final_fields
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, prefer_final_fields, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 import 'package:fetin/MyTextFormField.dart';
@@ -126,7 +126,7 @@ class EditPerfilUserState extends State<EditPerfilUser> {
                             child: Text(
                               'CPF: 111.222.333-44',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.grey,
                                 fontFamily: 'Raleway',
                                 fontSize: 20,
                                 fontWeight: FontWeight.w300,
